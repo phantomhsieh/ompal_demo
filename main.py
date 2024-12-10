6b43e69146fc464e6da984d553bb9f1aae50a77c
Phantom Hsieh <hsiehwenwei2@gmail.com>
2024-12-10 16:32:34 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import sounddevice as sd
+import numpy as np
+import wave
+
+# Title
+st.title("OMPAL - Inference API")
+
+# Text input
+st.subheader("Enter Text")
+text_input = st.text_area("Type your text here:", "")
+
+# Audio recording setup
+st.subheader("Record Audio (less than 10 seconds)")
